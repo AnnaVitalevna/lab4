c774f4efd9bba1c0dcba0fb68da48d37aa8c4941
Анна Белоусова <yanuta2000@mail.ru>
2018-12-25 19:56:53 +0600

    1st commit

---
+++ src/lab4/Lab4.java
@@ -0,0 +1,19 @@
+package lab4;
+public class Lab4 {
+
+  
+    public static void main(String[] args) {
+        String src = "D:\\Study\\IT\\SecondYear\\text.txt";
+        String dst = "D:\\Study\\IT\\SecondYear\\text1.txt";
+        FileManager fileManager;
+        try{
+            fileManager =new FileManager();
+            fileManager.copyFile(src,dst);
+            System.out.println("Копирование прошло успешно");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+    }
+    
+}
+}
